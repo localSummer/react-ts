1b4ff034c5188fa5a766fa4d852a83cd36f73df3
wangxingwen <wangxingwen@liepin.com>
2019-09-17 12:10:44 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {Provider} from 'mobx-react';
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
-import {stores} from './stores/index';
-import './utils/index';
-import './index.css';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
+import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import { stores } from './stores/index'
+import './utils/index'
+import './index.css'
 
 ReactDOM.render(
   <Provider {...stores}>
     <App />
   </Provider>,
   document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+)
+registerServiceWorker()
--- src/views/hooks/Index.tsx
+++ src/views/hooks/Index.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import { BgSore } from './store/BgStore';
-import Content from './components/Content';
-import Buttons from './components/Buttons';
-import GetState from './components/GetState';
+import * as React from 'react'
+import { BgSore } from './store/BgStore'
+import Content from './components/Content'
+import Buttons from './components/Buttons'
+import GetState from './components/GetState'
 
-const MokeRedux: React.FC= () => {
+const MokeRedux: React.FC = () => {
   return (
     <BgSore>
       <Content />
       <Buttons />
       <GetState />
     </BgSore>
-  );
-};
+  )
+}
 
-export default MokeRedux;
+export default MokeRedux
