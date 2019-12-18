8900328e06825b59dedc1a0228351c47af9791ff
wangxingwen <wangxingwen@liepin.com>
2019-12-18 15:47:31 +0800

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,35 +1,7 @@
-function extend<T, U>(first: T, second: U): T & U {
-  const result = {} as T & U
-  for (const id in first) {
-    if ((first as any)[id]) {
-      ;(result as any)[id] = (first as any)[id]
-    }
+function getValue(value: unknown): string {
+  if (value instanceof Date) {
+    // 类型保护
+    return value.toISOString()
   }
-  for (const id in second) {
-    if (!(result as any).hasOwnProperty(id)) {
-      ;(result as any)[id] = (second as any)[id]
-    }
-  }
-  return result
+  return String(value)
 }
-
-class Person {
-  constructor(public name: string) {}
-}
-
-interface ILoggable {
-  log(): void
-}
-
-class ConsoleLogger implements ILoggable {
-  public log() {
-    console.log('111')
-  }
-}
-
-const jim = extend<Person, ConsoleLogger>(
-  new Person('Jim'),
-  new ConsoleLogger()
-)
-
-console.log(jim.name, jim.log())
